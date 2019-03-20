16092e0be1e11a3bc79d1671eabd7dac5d3b8959
Brad (CV) <ongakukikai@gmail.com>
2019-03-20 08:55:35 -0400

    Updated and (Hopefully) fixed
    Code fixed for the current system. Added fixes to a few bugs and puts what used to be 6 or 7 programs into these few.

---
+++ analyze.py
@@ -0,0 +1,68 @@
+import readability
+import pickle
+import syntok.segmenter as segmenter
+subs = ['The_Donald','democrats','politics','bossfight']
+
+finals = {'The_Donald':'','democrats':'','politics':'','bossfight':''}
+
+for x in subs:
+    file = open(x,'rb')
+    print('opened')
+    z = pickle.load(file)
+    file.close()
+    
+    string = ''
+    for y in z:
+        string = string + '\n' + y[0]
+        print('thinking...')
+    finals[x] = string
+
+with open ('finals','wb') as file:
+        pickle.dump(finals,file)
+
+for x in finals:
+    string = x + ' Analysis: \n'
+    tokenized = '\n\n'.join(
+     '\n'.join(' '.join(token.value for token in sentence)
+        for sentence in paragraph)
+     for paragraph in segmenter.analyze(finals[x]))
+
+    print(tokenized)
+    results = readability.getmeasures(tokenized, lang='en')
+    with open(x+'.txt','a')as text_file:
+        text_file.write('Readability Indexes: \n')
+        print('next')
+        text_file.write('FleschReadingEase: ' + str(results['readability grades']['FleschReadingEase']) + ' \n')
+        print('next')
+        text_file.write('Kincaid: ' + str(results['readability grades']['Kincaid']) + ' \n')
+        print('next')
+        text_file.write('SMOGIndex: ' + str(results['readability grades']['SMOGIndex']) + ' \n')
+        print('next')
+        text_file.write('\n' + 'Sentence Structure Information: \n')
+        print('next')
+        text_file.write('Average characters per word: ' + str(results['sentence info']['characters_per_word']) + ' \n')
+        print('next')
+        text_file.write('Average syllables per word: ' + str(results['sentence info']['syll_per_word']) + ' \n')
+        print('next')
+        text_file.write('Average words per sentence: ' + str(results['sentence info']['words_per_sentence']) + ' \n')
+        print('next')
+        text_file.write('Average sentences per paragraph: ' + str(results['sentence info']['sentences_per_paragraph']) + ' \n')
+        print('next')
+        text_file.write('Type token ratio: ' + str(results['sentence info']['type_token_ratio']) + ' \n')
+        print('next')
+        text_file.write('Total characters: ' + str(results['sentence info']['characters']) + ' \n')
+        print('next')
+        text_file.write('Total syllables: ' + str(results['sentence info']['syllables']) + ' \n')
+        print('next')
+        text_file.write('Total words: ' + str(results['sentence info']['words']) + ' \n')
+        print('next')
+        text_file.write('Total sentences: ' + str(results['sentence info']['sentences']) + ' \n')
+        print('next')
+        text_file.write('Total paragraphs: ' + str(results['sentence info']['paragraphs']) + ' \n')
+        print('next')
+        text_file.write('Total long words: ' + str(results['sentence info']['long_words']) + ' \n')
+        print('next')
+        text_file.write('Total complex words: ' + str(results['sentence info']['complex_words']) + ' \n')
+        print('next')
+        text_file.write('Words to complex words ratio: ' + str(results['sentence info']['words']/results['sentence info']['complex_words']) + ' \n')
+        print('finished one')
--- redditInfo.py
+++ redditInfo.py
@@ -3,7 +3,7 @@ import pickle
 import time
 
 filename = 'SIPdata'
-subsList = ['republicans','democrats','politics','bossfight']
+subsList = ['The_Donald','democrats','politics','bossfight']
 try:
     infile = open(filename,'rb')
     data = pickle.load(infile)
@@ -16,28 +16,27 @@ except:
 
 reddit = praw.Reddit('SIPbot')
 
+def contained(post,sub)
+    for x in data[sub]['posts']:
+        if x == post:
+            return True
+    return False
+
+
 def collect(sub):
-    for post in reddit.subreddit(sub).hot(limit=2):
-        x = len(data[sub]['posts'])
-        if x == 0:
-            data[sub]['title'].append(post.title)
-            data[sub]['posts'].append(post.selftext)
-            p=0
-            for y in post.comments:
-                    data[sub]['comments'].append(post.comments[p].body)
-                    p = p+1
-        else:
-            if data[sub]['posts'][x-1] != post.selftext:
+    try:
+        for post in reddit.subreddit(sub).hot(limit=2):
+            if not contained(post,sub):
                 data[sub]['title'].append(post.title)
                 data[sub]['posts'].append(post.selftext)
                 p=0
-                for y in post.comments:
+                for y in post.comments.replace_more(limit=0):
                     data[sub]['comments'].append(post.comments[p].body)
                     p = p+1
 
 while (1<2):
     for i in subsList:
-        time.sleep(600)
+        time.sleep(2)
         collect(i)
         with open(filename,'wb') as file:
             pickle.dump(data,file)
